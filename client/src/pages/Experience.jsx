50bcde91af5badffbfffd817ac21650cd8f89ff0
JuanDavidVasquez <67479047+JuanDavidVasquez@users.noreply.github.com>
2024-02-28 16:39:26 -0500

    1639

--- client/src/pages/Experience.jsx
+++ client/src/pages/Experience.jsx
@@ -5,47 +5,55 @@ import 'react-vertical-timeline-component/style.min.css';
 export default function Experience() {
   return (
     <div className='w-full py-20 bg-black bg-opacity-80 text-white'>
-      <h1 className='montserrat text-9xl text-center'>Experience</h1>
+      <h1 className='montserrat text-9xl text-center pb-10'>Experience</h1>
       <VerticalTimeline>
         <VerticalTimelineElement
           className="vertical-timeline-element text-gray-500"
           date="2020"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          iconStyle={{background: 'url("img/icons/iconReact.png")', backgroundSize: "cover" }}
         >
-          <h3 className="vertical-timeline-element-title">Event 1</h3>
-          <p>Some information about the event in 2020.</p>
+          <h3 className="vertical-timeline-element-title">CRM - Website</h3>
+          <p>CRM creation and administration for collection companies.</p>
+          <p>Website Freelance</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element text-gray-500"
           date="2021"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          iconStyle={{background: 'url("img/icons/iconJS.gif")', backgroundSize: "cover", backgroundPosition:"center" }}
         >
-          <h3 className="vertical-timeline-element-title">Event 2</h3>
-          <p>Some information about the event in 2021.</p>
+          <h3 className="vertical-timeline-element-title">CRM</h3>
+          <p>CRM creation and administration for Vanti allies.</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element text-gray-500"
           date="2022"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          iconStyle={{background: 'url("img/icons/iconLaravel.png")', backgroundSize: "cover" }}
         >
-          <h3 className="vertical-timeline-element-title">Event 3</h3>
-          <p>Some information about the event in 2022.</p>
+          <h3 className="vertical-timeline-element-title">CRM</h3>
+          <p>CRM creation and administration for Claro and Telefonica BPO allies - Movistar.</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element text-gray-500"
           date="2023"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          iconStyle={{background: 'url("img/icons/iconJava.gif")', backgroundSize: "cover" }}
         >
-          <h3 className="vertical-timeline-element-title">Event 4</h3>
-          <p>Some information about the event in 2023.</p>
+          <h3 className="vertical-timeline-element-title">Intranet - Coffe - Soy Cultura Local</h3>
+          <p>Intranet: Creation and implementation of the Auth and Time Manager modules.</p>
+          <p>Coffee: Creation and implementation of the coffee shop sales application</p>
+          <p>Landing page creation I am local culture Bogotá 2023</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element text-gray-500"
           date="2024"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          iconStyle={{background: 'url("img/icons/iconMysql.png")', backgroundSize: "cover" }}
         >
-          <h3 className="vertical-timeline-element-title">Event 5</h3>
-          <p>Some information about the event in 2024.</p>
+          <h3 className="vertical-timeline-element-title">Intranet</h3>
+          <p>Modules: <br/>RRHH, Cosecha, App-Salidas</p>
+          <p>
+            Creation of the HHRR module, app to administer and manage the selection and hiring process.
+            Cration module harvests, administers and manages processes related to payments from allies
+            Creation of an exit module, which manages the flow of personnel who present something new and leave or enter outside their usual hours.
+          </p>
         </VerticalTimelineElement>
       </VerticalTimeline>
     </div>
